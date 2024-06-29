baa0c8e15c2594e4876384eebf2d34162e9e48c6
8ctopotamus <zylo.codes@gmail.com>
2024-06-29 13:26:09 -0500

    wip: linked list

---
+++ misc/linked-list.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct s_book {
+	char title[64];
+	int pages;
+	struct s_book *next;
+};
+
+typedef struct s_book Book; 
+
+Book *first;
+int numBooks;
+
+void addBook(char *title, int pages) {
+	Book *p, *new;
+
+	if (!first) {
+		p = malloc(sizeof(sizeof(Book)));
+		memset(p, 0, sizeof(Book));
+		numBooks++;
+		strncpy(p->title, title, 63);
+		p->pages = pages;
+		p->next = 0;
+		first = p;
+
+		return;
+	} 
+
+	for (p = first; p->next; p++);
+
+	new = malloc(sizeof(sizeof(Book)));
+	memset(new, 0, sizeof(Book));
+	numBooks++;
+	strncpy(new->title, title, 63);
+	new->pages = pages;
+	new->next = 0;
+	
+	p->next = new;
+
+	return;
+}
+
+int main() {
+	first = 0;
+	numBooks = 0;
+
+	addBook("The Hobbit", 3500);
+	printf("%s\n", first->title);
+
+	return 0;
+}
---
+++ misc/malloc.c
@@ -0,0 +1,21 @@
+// dynamic memory allocation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	char *name;
+
+	name = malloc(32);
+
+	printf("What is your name?");
+	scanf("%s", name);
+
+	printf("Hello %s\n", name);
+
+	free(name);
+	name = NULL;
+
+	return 0;
+}
